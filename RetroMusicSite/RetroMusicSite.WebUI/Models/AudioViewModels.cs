c73f169674ece4504a7765cddae6a9b976f75420
IvanetsSrhei <ivanets1992@gmail.com>
2016-11-03 19:38:18 +0300

    Add class AudioViewModels

---
+++ RetroMusicSite/RetroMusicSite.WebUI/Models/AudioViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using RetroMusicSite.Domain.Entities;
+
+namespace RetroMusicSite.WebUI.Models
+{
+    public class AudioViewModels
+    {
+        public IEnumerable<Audio> Audios { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
