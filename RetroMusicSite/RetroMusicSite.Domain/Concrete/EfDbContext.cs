e2e2030d3006bca5b06f21e3baf2c4b7eef11708
IvanetsSrhei <ivanets1992@gmail.com>
2016-11-03 17:10:45 +0300

    Add folder Concrete for implementation DbContext

---
+++ RetroMusicSite/RetroMusicSite.Domain/Concrete/EfDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using RetroMusicSite.Domain.Entities;
+
+
+namespace RetroMusicSite.Domain.Concrete
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Audio> Audios { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
