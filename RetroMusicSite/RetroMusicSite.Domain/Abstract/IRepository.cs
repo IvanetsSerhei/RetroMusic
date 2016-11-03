5eebd14dca4497e53124f4040b8caf9b957e48fc
IvanetsSrhei <ivanets1992@gmail.com>
2016-11-03 16:32:10 +0300

    Add new folder Abstract for implementation IRepository.

---
+++ RetroMusicSite/RetroMusicSite.Domain/Abstract/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using RetroMusicSite.Domain.Entities;
+
+namespace RetroMusicSite.Domain.Abstract
+{
+    public interface IRepository
+    {
+        IEnumerable<Artist> Artists { get; }
+        IEnumerable<Audio> Audios { get; }
+    }
+}
