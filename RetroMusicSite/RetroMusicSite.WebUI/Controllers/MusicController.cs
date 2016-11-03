470774745d0ed23fb64eebcde41f44adb65f6494
IvanetsSrhei <ivanets1992@gmail.com>
2016-11-03 16:42:41 +0300

    Add MusicController and Views Audio.

---
+++ RetroMusicSite/RetroMusicSite.WebUI/Controllers/MusicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using RetroMusicSite.Domain.Abstract;
+
+namespace RetroMusicSite.WebUI.Controllers
+{
+    public class MusicController : Controller
+    {
+        private IRepository _repository;
+
+        public MusicController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public ViewResult Audio()
+        {
+            return View(_repository.Audios);
+        }
+
+    }
+}
