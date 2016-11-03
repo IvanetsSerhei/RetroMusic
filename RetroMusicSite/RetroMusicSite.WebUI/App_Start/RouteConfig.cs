b270c3dddbff57a066f9d47380433e47fc648359
IvanetsSrhei <ivanets1992@gmail.com>
2016-11-03 19:45:38 +0300

    update RouteConfig

--- RetroMusicSite/RetroMusicSite.WebUI/App_Start/RouteConfig.cs
+++ RetroMusicSite/RetroMusicSite.WebUI/App_Start/RouteConfig.cs
@@ -10,10 +10,17 @@ namespace RetroMusicSite.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Music", action = "Audio" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Music", action = "Audio", id = UrlParameter.Optional }
             );
+         
         }
     }
 }
