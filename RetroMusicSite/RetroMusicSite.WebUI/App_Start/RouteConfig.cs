8ea0d59507eee1e927921c7f2d0cb45ea6f5a5cc
IvanetsSrhei <ivanets1992@gmail.com>
2016-11-06 19:44:41 +0300

    update cshtml and routeConfig

--- RetroMusicSite/RetroMusicSite.WebUI/App_Start/RouteConfig.cs
+++ RetroMusicSite/RetroMusicSite.WebUI/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Security.Policy;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RetroMusicSite.WebUI
@@ -10,17 +11,19 @@ namespace RetroMusicSite.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new { controller = "Music", action = "Audio" }
-            );
+            name: "Default2",
+            url: "{artistId}",
+            defaults: new { controller = "Music", action = "Audio" },
+            constraints: new { artistId = "\\d+" }
+        );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Music", action = "Audio", id = UrlParameter.Optional }
-            );
-         
+            name: "Default",
+            url: "{category}",
+            defaults: new { controller = "Music", action = "Artist", category = (string)null }            
+        );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
