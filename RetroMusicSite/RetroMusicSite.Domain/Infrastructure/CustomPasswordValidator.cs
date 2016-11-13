3cb1cb8a23082ba519ddaf195be22baaf463c6c0
IvanetsSrhei <ivanets1992@gmail.com>
2016-11-13 13:27:09 +0300

    add custom classes validation for add user

---
+++ RetroMusicSite/RetroMusicSite.Domain/Infrastructure/CustomPasswordValidator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace RetroMusicSite.WebUI.Infrastructure.Identity
+{
+    public class CustomPasswordValidator : PasswordValidator
+    {
+        public override async Task<IdentityResult> ValidateAsync(string pass)
+        {
+            List<string> errors = new List<string>();
+
+            string passPattern = @"^[a-zA-Z0-9а-яА-Я]+$";
+
+            if (String.IsNullOrEmpty(pass))
+                errors.Add("Пароль не может быть пустым,");
+
+            else
+            {
+                if (pass.Contains("12345"))
+                {
+                    errors.Add("Пароль не должен содержать последовательности чисел.");
+                }
+
+                if (pass.Contains("qwert") || pass.Contains("Qwert") || pass.Contains("QWERT"))
+                {
+                    errors.Add("Пароль не должен содержать последовательности букв <qwert>.");
+                }
+
+                if (!Regex.IsMatch(pass, passPattern))
+                    errors.Add("В пароле разрешается указывать " +
+                               "буквы английского или русского языков, и цифры. ");
+
+                if (pass.Length < 6)
+                    errors.Add("Пароль должен быть не меньше 6 символов");
+
+                if(pass.Equals(pass.ToLowerInvariant()))
+                     errors.Add("Пароль должен содержать хотябы 1 заглавный символ");          
+            }
+
+            if (errors.Count > 0)
+                return IdentityResult.Failed(errors.ToArray());
+
+            return IdentityResult.Success;
+        }
+    }
+}
---
+++ RetroMusicSite/RetroMusicSite.Domain/Infrastructure/CustomUserValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using RetroMusicSite.Domain.Entities.Identity;
+
+namespace RetroMusicSite.Domain.Infrastructure
+{
+    class CustomUserValidator : IIdentityValidator<AppUser>
+    {
+        public async Task<IdentityResult> ValidateAsync(AppUser item)
+        {
+            List<string> errors = new List<string>();
+
+            if (String.IsNullOrEmpty(item.UserName.Trim()))
+                errors.Add("Вы указали пустое имя.");
+
+            string userNamePattern = @"^[a-zA-Z0-9а-яА-Я]+$";
+
+            if (!Regex.IsMatch(item.UserName, userNamePattern))
+                errors.Add("В имени разрешается указывать буквы английского или русского языков, и цифры");
+
+            if (String.IsNullOrEmpty(item.Email.Trim()))
+                errors.Add("Вы не указали email адресс.");
+
+            string userEmailPattern = "^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
+            if (!Regex.IsMatch(item.Email, userEmailPattern))
+                errors.Add("Введите корректный email адресс");
+
+            if (errors.Count > 0)
+                return IdentityResult.Failed(errors.ToArray());
+
+            return IdentityResult.Success;
+        }
+    }
+}
